3eb556c5d711b0f8aa20869d894c6d01e2caff34
zkyseu <118790294+zkyseu@users.noreply.github.com>
2023-09-23 10:20:11 +0800

    Update resnet_18_culane.py

--- configs/resnet_18_culane.py
+++ configs/resnet_18_culane.py
@@ -24,7 +24,7 @@ model = dict(
         type='ResNet',
         depth=18,
         num_stages=4,
-        out_indices=(1,2,3),
+        out_indices=(0,1,2,3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=False),
         norm_eval=True,
